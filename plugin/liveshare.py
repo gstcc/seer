42c94ba2b25d29a5b719b574e12926ea7fa647e6
gstcc <gustavcarlsson555@gmail.com>
2025-06-13 14:57:19 +0200

    Some small updates

--- plugin/liveshare.py
+++ plugin/liveshare.py
@@ -7,11 +7,23 @@ class LiveShare(object):
     def __init__(self, nvim) -> None:
         self.nvim = nvim
 
+    @pynvim.command("StartServer", nargs="*")
+    def start_server(self, args):
+        self.nvim.out_write(f"Started server\n")
+
+    @pynvim.command("StopServer", nargs="*")
+    def stop_server(self, args):
+        self.nvim.out_write(f"Stopped server\n")
+
+    @pynvim.command("JoinSession", nargs="*")
+    def join_session(self, args):
+        self.nvim.out_write(f"Joining session witha args {args}\n")
+
     @pynvim.autocmd("BufEnter", pattern="*.py", eval='expand("<afile>")', sync=True)
     def on_buf_enter(self, filename: str):
         self.nvim.out_write("testplugin is in " + filename + "\n")
 
-    @pynvim.autocmd("TextChanged,TextChangedI", pattern="*.py", sync=True)
+    @pynvim.autocmd("TextChanged,TextChangedI", pattern="*", sync=True)
     def on_buffer_change(self):
         # Get current buffer number
         buf = self.nvim.current.buffer
